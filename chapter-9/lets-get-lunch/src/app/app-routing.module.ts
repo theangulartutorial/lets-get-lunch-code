c525c035e08d2070af42fe477233f5b2d3ef0549
Adam Morgan <atom.morgan@gmail.com>
2018-01-20 10:38:25 -0500

    Add ch-9 code

--- chapter-9/lets-get-lunch/src/app/app-routing.module.ts
+++ chapter-9/lets-get-lunch/src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'signup', loadChildren: 'app/signup/signup.module#SignupModule' }
 ];
 
 @NgModule({
--- chapter-9/lets-get-lunch/src/app/app.module.ts
+++ chapter-9/lets-get-lunch/src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Ng2Webstorage } from 'ngx-webstorage';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './services/auth/auth.service';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -14,9 +18,11 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    Ng2Webstorage
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
