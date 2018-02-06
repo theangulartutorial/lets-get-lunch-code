59c357dc3e8dca0732dba4252a2c9d993e8baf06
Adam Morgan <atom.morgan@gmail.com>
2018-02-06 08:33:13 -0500

    Migrate ch-14 code to ch-15

---
+++ chapter-15/lets-get-lunch/src/app/event/event.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from 'ng-pick-datetime';
+
+import { EventRoutingModule } from './event-routing.module';
+import { EventCreateComponent } from './event-create/event-create.component';
+import { EventsService } from '../services/events/events.service';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    EventRoutingModule,
+    ReactiveFormsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule
+  ],
+  declarations: [EventCreateComponent],
+  providers: [EventsService]
+})
+export class EventModule { }
