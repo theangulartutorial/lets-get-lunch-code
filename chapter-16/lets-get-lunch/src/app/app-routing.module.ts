667c9cd842739022427a3374c3a980eb3898a9a9
Adam Morgan <atom.morgan@gmail.com>
2018-02-08 08:38:04 -0500

    Migrate chapter-15 code to chapter-16

---
+++ chapter-16/lets-get-lunch/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'signup', loadChildren: 'app/signup/signup.module#SignupModule' },
+  { path: 'dashboard', loadChildren: 'app/dashboard/dashboard.module#DashboardModule' },
+  { path: 'login', loadChildren: 'app/login/login.module#LoginModule' },
+  { path: 'event', loadChildren: 'app/event/event.module#EventModule' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
