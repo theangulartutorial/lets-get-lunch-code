60a717181727f1788ee5abde9135af118d3436ee
Adam Morgan <atom.morgan@gmail.com>
2018-02-21 09:20:16 -0500

    Migrate chapter-21 code to chapter-22

---
+++ chapter-22/lets-get-lunch/src/app/guards/auth/auth.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      // TODO Redirect to login
+      this.router.navigate(['/']);
+      return false;
+    }
+  }
+}
