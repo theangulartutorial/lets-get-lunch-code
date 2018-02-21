ffc0b40f31577387ab8ed8ba3cc2b5882ea66515
Adam Morgan <atom.morgan@gmail.com>
2018-02-21 17:15:15 -0500

    Add chapter-22 code

--- chapter-22/lets-get-lunch/src/app/event/event-routing.module.ts
+++ chapter-22/lets-get-lunch/src/app/event/event-routing.module.ts
@@ -5,15 +5,17 @@ import { AuthGuard } from '../guards/auth/auth.guard';
 import { EventCreateComponent } from './event-create/event-create.component';
 import { EventViewComponent } from './event-view/event-view.component';
 import { EventUpdateComponent } from './event-update/event-update.component';
+import { EventGuard } from '../guards/event/event.guard';
 
 const routes: Routes = [
   { path: '', component: EventCreateComponent, canActivate: [AuthGuard] },
   { path: ':id', component: EventViewComponent, canActivate: [AuthGuard] },
-  { path: ':id/update', component: EventUpdateComponent, canActivate: [AuthGuard] }
+  { path: ':id/update', component: EventUpdateComponent, canActivate: [EventGuard, AuthGuard] }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [EventGuard]
 })
 export class EventRoutingModule { }
