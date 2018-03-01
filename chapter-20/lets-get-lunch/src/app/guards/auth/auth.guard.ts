26d4a988291324fdaaf6860f618ee4b72ff0f89a
Adam Morgan <atom.morgan@gmail.com>
2018-02-28 21:24:10 -0500

    Add chapter-11 migrations

--- chapter-20/lets-get-lunch/src/app/guards/auth/auth.guard.ts
+++ chapter-20/lets-get-lunch/src/app/guards/auth/auth.guard.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../../services/auth/auth.service';
 
